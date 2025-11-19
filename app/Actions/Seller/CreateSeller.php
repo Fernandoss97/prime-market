9fbd59159488f41a706d225487a0a29fa1c1908f
Fernandoss97 <fsnfernando97@gmail.com>
2025-11-18 21:43:14 -0300

    setting up roles

---
+++ app/Actions/Seller/CreateSeller.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Actions\Seller;
+
+use Lorisleiva\Actions\Concerns\AsAction;
+
+class CreateSeller
+{
+    use AsAction;
+
+    public function handle()
+    {
+        // ...
+    }
+}
