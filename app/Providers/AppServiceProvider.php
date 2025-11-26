3ec1a4ce8db1cef651d553d708d9cb907814de85
Fernandoss97 <fsnfernando97@gmail.com>
2025-11-26 20:27:08 -0300

    feat: configure application services in AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -21,10 +23,25 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         $this->configModels();
+        $this->configCommands();
+        $this->configResources();
     }
 
     protected function configModels(): void
     {
         Model::unguard();
+        Model::shouldBeStrict();
+    }
+
+    protected function configCommands(): void
+    {
+        DB::prohibitDestructiveCommands(
+            app()->isProduction(),
+        );
+    }
+
+    public function configResources(): void
+    {
+        JsonResource::withoutWrapping();
     }
 }
